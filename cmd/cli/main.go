9908aaef308b2fb65cb92e4e60c89eb43b4e0fa7
mproni <mpronin.vostok@gmail.com>
2024-07-24 22:10:34 +0500

    нужно CGO для корректной компиляции
    Добавлена вставка, но в целом код не работает. Жалуется на ошибку, что скомпилировался без CGO.
    В дальнейшем нужно поправить, а также изменить вставку, чтобы брались значения из os.Args.

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -13,4 +15,23 @@ func main() {
 		log.Fatal(err)
 	}
 	defer db.Close()
+
+	createTableSQL := `CREATE TABLE IF NOT EXISTS notes (
+		        "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		        "title" TEXT,
+		        "content" TEXT
+		    );`
+
+	_, err = db.Exec(createTableSQL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	insertNoteSQL := `INSERT INTO notes (title, content) VALUES (?, ?)`
+	statement, err := db.Prepare(insertNoteSQL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// всё не работает, потому что нужно CGO для компиляции.
+	fmt.Println(len(os.Args), os.Args[0], os.Args[1])
 }
