ac7784df4c8dbda92c13c77058dcb9c2a602648c
mproni <mpronin.vostok@gmail.com>
2024-07-25 13:28:23 +0500

    now it's working
    это работает, но хуже кода я в жизни не видел. нужно понять, как открыть соединение с таблицей один раз и дальше использовать это соединение не открывая его 100500 миллионов раз.
    думаю мне нужны методы и рефакторинг аген

--- internal/database/create.go
+++ internal/database/create.go
@@ -7,14 +7,13 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-var db *sql.DB
-
 func CreateTableSQL() {
 	db, err := sql.Open("sqlite3", "./notes.db")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
+	// ну ты и дурак, конечно.
+	//	defer db.Close()
 
 	createTableSQL := `CREATE TABLE IF NOT EXISTS notes (
 		        "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
--- internal/database/queries.go
+++ internal/database/queries.go
@@ -1,25 +1,36 @@
 package database
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 )
 
 func AddSQL() {
+	db, err := sql.Open("sqlite3", "./notes.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 	query := `INSERT INTO notes (title, content) VALUES (?, ?)`
-	_, err := db.Exec(query, os.Args[2], os.Args[3])
+	_, err = db.Exec(query, os.Args[2], os.Args[3])
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 func ReadSQL() {
+	db, err := sql.Open("sqlite3", "./notes.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 	var id int
 	var title string
 	var content string
 	query := `SELECT id, title, content FROM notes WHERE id = ?`
-	err := db.QueryRow(query, os.Args[2]).Scan(&id, &title, &content)
+	err = db.QueryRow(query, os.Args[2]).Scan(&id, &title, &content)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -27,6 +38,11 @@ func ReadSQL() {
 }
 
 func ReadAllSQL() {
+	db, err := sql.Open("sqlite3", "./notes.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 	query := `SELECT id, title, content FROM notes ORDER BY id`
 	row, err := db.Query(query)
 	if err != nil {
@@ -44,32 +60,52 @@ func ReadAllSQL() {
 }
 
 func UpdateSQL() {
+	db, err := sql.Open("sqlite3", "./notes.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 	query := `UPDATE notes SET title = ?, content = ? WHERE id = ?`
-	_, err := db.Exec(query, os.Args[3], os.Args[4], os.Args[2])
+	_, err = db.Exec(query, os.Args[3], os.Args[4], os.Args[2])
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 func UpdateTitleSQL() {
+	db, err := sql.Open("sqlite3", "./notes.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 	query := `UPDATE notes SET title = ? WHERE id = ?`
-	_, err := db.Exec(query, os.Args[3], os.Args[2])
+	_, err = db.Exec(query, os.Args[3], os.Args[2])
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 func UpdateContentSQL() {
+	db, err := sql.Open("sqlite3", "./notes.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 	query := `UPDATE notes SET content = ? WHERE id = ?`
-	_, err := db.Exec(query, os.Args[3], os.Args[2])
+	_, err = db.Exec(query, os.Args[3], os.Args[2])
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 func DeleteSQL() {
+	db, err := sql.Open("sqlite3", "./notes.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 	query := `DELETE FROM notes WHERE id = ?`
-	_, err := db.Exec(query, os.Args[2])
+	_, err = db.Exec(query, os.Args[2])
 	if err != nil {
 		log.Fatal(err)
 	}
