6a3eedb1dd7f616b94bd16ef2d9920965929bb95
mproni <mpronin.vostok@gmail.com>
2024-07-25 11:56:03 +0500

    divide and rule
    подготовка к будущему разделению файла main.go на "проверка аргументов командной строки", "проверка (создание) таблицы", "CRUD операции".
    args.go - done

---
+++ internal/handler/handlers.go
@@ -0,0 +1,33 @@
+package handler
+
+func Add() {
+	//TODO
+}
+
+func Read() {
+	//TODO
+}
+
+func ReadAll() {
+	//TODO
+}
+
+func Update() {
+	//TODO
+}
+
+func UpdateTitle() {
+	//TODO
+}
+
+func UpdateContent() {
+	//TODO
+}
+
+func Delete() {
+	//TODO
+}
+
+func Help() {
+	//TODO
+}
